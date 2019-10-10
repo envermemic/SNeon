146ae9d7b0ceb633df5aac833c901d5027d97921
Enver <enver.memic80@gmail.com>
2019-10-10 21:14:16 +0200

    Initial Commit

---
+++ SNeon/SNeon.h
@@ -0,0 +1,19 @@
+//
+//  SNeon.h
+//  SNeon
+//
+//  Created by Tech 387 on 10/10/2019.
+//  Copyright © 2019 Tech 387. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SNeon.
+FOUNDATION_EXPORT double SNeonVersionNumber;
+
+//! Project version string for SNeon.
+FOUNDATION_EXPORT const unsigned char SNeonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SNeon/PublicHeader.h>
+
+
