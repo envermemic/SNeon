262fb1ef8d676bd2d1cbaacb4d67ffd56484d5f8
Enver <enver.memic80@gmail.com>
2019-10-16 16:07:38 +0200

    Update SButton.swift

--- SNeon/SButton.swift
+++ SNeon/SButton.swift
@@ -12,36 +12,97 @@ import MaterialComponents
 
 public protocol SButtonDelegate { func s_button_did_tap() }
 
-
 /// Use for components with content
 /// for resize by content or fix size
 public enum FLType {
+    
     /// flexibile by title
     case fl
+    
     /// fixed
     case fx
 }
 
+///
+public enum Pos {
+    case left
+    case right
+    case top
+    case bottom
+    case center
+}
+
+
 public class SButton: UIView {
     
-    public var delegate: SButtonDelegate?
-    private var flType: FLType = .fl
-    private var btnTitle: String = ""
-    private var upprcassed: Bool = true
-    private var radius: CGFloat = 0
-    private var vibration: Vibr?
+    private let off: CGFloat = 12
+    private let ins: CGFloat = 8
+    
+    enum SButtonKind {
+        case icon
+        case title
+        case combo
+        case none
+    }
+    
+    
+    // BASE ATTRIBUTTES
+    
+    ///
+    private var btnTitle: String?	
+    
+    ///
+    private var iconName: String?
+    
+    /// 
     private var mdcButton = MDCButton()
+    
+    ///
+    private var lb = UILabel()
+    
+    /// 
+    private var iv = UIImageView()
+       
+    /// button instance identifier
+    private var id = ""
+    
+    
+    // CONFIGURATION
+    
+    ///
+    private var flType: FLType = .fl
+    
+    ///
+    private var kind: SButtonKind = .none
+    
+    ///
+    private var upprcassed: Bool = true
+    
+    ///
+    private var radius: CGFloat = 0
+    
+    ///
+    private var groupTo: Pos = .left
+    
+    // OPTIONS
+    
+    ///
+    private var vibration: Vibr?
+    
+    ///
+    public var delegate: SButtonDelegate?
+    
     
     ///
     public init(_ withBlink: Bool = false, vibration: Vibr? = nil) {
         super.init(frame: CGRect.zero)
         self.vibration = vibration
-        mdcButton.setTitle(" ", for: .normal)
-        self.setSize()
+        mdcButton.setTitle("", for: .normal)
         if withBlink { mdcButton.enableRippleBehavior = false }
         mdcButton.addTarget(self, action: #selector(didTap), for: .touchUpInside)
         self.clipsToBounds = true
         addSubview(mdcButton)
+        defaultSet()
     }
     
     ///
@@ -53,64 +114,131 @@ public class SButton: UIView {
         self.layout()
     }
     
+    
+    // =======================  GETTERS AND SETTERS  =============================
+    
+    /// button title value, getter and setter
+    public var title: String? {
+        get { return btnTitle }
+        set { self.btnTitle = newValue; titleDidSet(); layout() }
+    }
+    
+    /// icon getter and setter
+    /// rendering mode by default **.alwaysOriginal**
+    public var icon: String? {
+        get { return iconName }
+        set { self.iconName = newValue; iconDidSet(.alwaysOriginal); layout() }
+    }
+    
+    
+    /// set icon image
+    /// - Parameter icon: icon name
+    /// - Parameter withRenderingMode: rendering mode by default **.alwaysOriginal**
+    public func setIcon(icon: String?, withRenderingMode: UIImage.RenderingMode = .alwaysOriginal) {
+        self.iconName = icon; iconDidSet(withRenderingMode); layout()
+    }
+    
     /// vibration when click
     public var vibr: Vibr? {
         get { return vibration }
         set { vibration = newValue }
     }
     
-    /// set color scheme
-    /// - Parameter title: title text color UIColor (default is black)
-    /// - Parameter bckg: background color required UIColor
-    public func colors(title: UIColor = .black, bckg: UIColor) {
-        backgroundColor = .clear
-        mdcButton.backgroundColor = bckg
-        mdcButton.setTitleColor(title, for: .normal)
+    /// uppercssed letters
+    public var isUppercassed: Bool {
+        get { return upprcassed }
+        set { upprcassed = newValue; uppercassedDidSet(); layout() }
     }
     
-    /// setup global corner radius and border
-    ///
-    /// radius value less than zero will be seted to -1 and will be used
-    /// height/2 for corners, from 0 to height/2 will be used regular, bigger than
-    /// height/2 will be used as height/2.
-    ///
-    /// parameter **c** (color) set boreder color using with param **w** as border width.
-    /// if parameter c (color) is nil, border will be removed.
-    ///
-    /// - Parameter radius: CGFloat
-    /// - Parameter c: UIColor optional
-    /// - Parameter w: CGFloat
-    public func border(_ radius: CGFloat, c: UIColor? = nil, w: CGFloat = 0) {
-        self.radius = radius < 0 ? -1 : radius > frame.height / 2 ? frame.height / 2 : radius
-        if let borderColor = c {
-            layer.borderColor = borderColor.cgColor
-            layer.borderWidth = w
+    /// title position set and get
+    public var group: Pos {
+        get { return groupTo }
+        set { self.groupTo = newValue; layout() }
+    }
+    
+    // =============================  CONFIG FUNCTIONS  ============================
+    // konfigurise componentu nakon setanja nekog parametra
+    
+    /// config component after seted or unseted icon
+    /// remove from superview, or add to superview icon view **iv**
+    /// set component kind
+    /// set or unset icon
+    private func iconDidSet(_ mode: UIImage.RenderingMode) {
+        if let icon = iconName {
+            if self.iv.superview == nil { addSubview(iv) }
+            self.kind = btnTitle != nil ? .combo : .icon
+            self.iv.image = UIImage(named: icon)?.withRenderingMode(mode)
         } else {
-            layer.borderColor = nil
-            layer.borderWidth = 0
+            if self.iv.superview != nil { iv.removeFromSuperview() }
+            self.kind = btnTitle != nil ? .title : .none
+        }
+    } 
+    
+    ///
+    private func titleDidSet() {
+        if let title = btnTitle {
+            if lb.superview == nil { addSubview(lb) }
+            lb.text = upprcassed ? title.uppercased() : title
+            self.kind = iconName != nil ? .combo : .title
+        } else {
+            if self.lb.superview != nil { lb.removeFromSuperview() }
+            lb.text = ""
+            self.kind = iconName != nil ? .icon : .none
         }
     }
     
-    /// set size
-    /// by default will be seted width by text, height 48
-    /// with params will be seted manual
-    /// - Parameter w: manual width without resizable **CGFloat**
-    /// - Parameter h: manual height **CGFloat**
-    public func setSize(_ w: CGFloat? = nil, _ h: CGFloat? = nil) {
-        // set width and flex type
-        if let width = w { flType = .fx; frame.size.width = width }
-        else {
+    ///
+    private func uppercassedDidSet() { if btnTitle != nil { titleDidSet() }}
+    
+    
+    // =============================  SET FUNCTIONS  ===========================
+    
+    public func defaultSet() {
+        frame.size.width = 48
+        frame.size.height = 48
+        self.lb.textColor = .black
+        self.iv.tintColor = .black
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 0
+        self.layer.borderColor = nil
+        self.layer.borderWidth = 0
+        layout()
+    }
+    
+    public func set(titleColor: UIColor? = nil,
+                    tintColor: UIColor? = nil,
+                    backgroundColor: UIColor? = nil,
+                    radius: CGFloat? = nil,
+                    borderColor: UIColor? = nil,
+                    borderWidth: CGFloat? = nil,
+                    w: CGFloat? = nil,
+                    h: CGFloat? = nil) {
+        
+        func widthByText() {
             flType = .fl
-            let textW: CGFloat = btnTitle.width(font: mdcButton.titleLabel!.font)
-            frame.size.width = textW + 24
+            lb.frame.size.width = 24
+            if let txt = btnTitle {
+                let textW: CGFloat = txt.width(font: lb.font)
+                lb.frame.size.width += textW
+            }
+            lb.frame.size.height = 24
         }
         
-        // set height
-        if let height = h { frame.size.height = height }
-        else { frame.size.height = 48 }
-        resetTitle()
+        func setupRadius(radius: CGFloat) {
+            self.radius = radius < 0 ? -1 : radius > frame.height / 2 ? frame.height / 2 : radius
+            self.layer.cornerRadius = self.radius
+        }
+        
+        if let width = w { flType = .fx; frame.size.width = width } else { widthByText() }
+        if let height = h { frame.size.height = height } else { frame.size.height = 48 }
+        if let titleColor = titleColor { self.lb.textColor = titleColor }
+        if let tintColor = tintColor { self.iv.tintColor = tintColor }
+        if let background = backgroundColor { self.backgroundColor = background }
+        if let rad = radius { setupRadius(radius: rad) }
+        if let borderC = borderColor { self.layer.borderColor = borderC.cgColor }
+        if let borderW = borderWidth { self.layer.borderWidth = borderW }
+        layout()
     }
-    
     
     /// add target to button
     /// - Parameter target: Any?
@@ -125,45 +253,32 @@ public class SButton: UIView {
         delegate?.s_button_did_tap()
     }
     
-    /// button title value, getter ansd setter
-    public var title: String? {
-        get { return btnTitle }
-        set {
-            if let text = newValue { self.btnTitle = text }
-            else { self.btnTitle = "" }
-            resetTitle()
-        }
-    }
-    
-    /// uppercssed letters
-    public var isUppercassed: Bool {
-        get { return upprcassed }
-        set {
-            upprcassed = newValue
-            resetTitle()
-        }
-    }
-    
-    /// resetup title
-    private func resetTitle() {
-        let ttl = upprcassed ? btnTitle.uppercased() : btnTitle
-        mdcButton.setTitle(ttl, for: .normal)
-        layout()
-    }
-    
     /// component layout
     private func layout() {
-        if flType == .fl {
-            let textW: CGFloat = btnTitle.width(font: mdcButton.titleLabel!.font)
-            frame.size.width = textW + 24
+        
+        // calculate place for subcomponents
+        func place() -> CGFloat {
+            if kind == .combo { return 24 + ins + lb.width }
+            else if kind == .title { return lb.width }
+            return 24
         }
-        if radius == 0 { layer.cornerRadius = 0 }
-        else if radius < 0 { layer.cornerRadius = frame.height / 2 }
-        else {
-            if radius > frame.size.width / 2 { layer.cornerRadius = frame.height / 2 }
-            else { layer.cornerRadius = radius }
+        
+        // determinate edge for layouts
+        let edge: Edge = groupTo == .right ? .right : .left
+        // determinate start X offset
+        var offs: CGFloat = flType == .fl ? off : groupTo == .right || groupTo == .left ? off : (frame.width - place()) / 2
+        // get active items
+        let items = kind == .title ? [lb] : kind == .icon ? [iv] : [iv, lb]
+        // set items
+        for item in items {
+            item.to_side(edge, pad: offs)
+            offs += item.width + ins
         }
+        // calculate flexibile size
+        if flType == .fl { frame.size.width = offs - ins + off }
+        // mdc button layout
         mdcButton.fillSuperview()
     }
 }
+
 
