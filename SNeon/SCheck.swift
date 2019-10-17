ef89f72f747f2f09a6c26a386de656b33f869bd8
Enver <enver.memic80@gmail.com>
2019-10-17 17:42:57 +0200

    Update SCheck.swift

--- SNeon/SCheck.swift
+++ SNeon/SCheck.swift
@@ -21,14 +21,25 @@ public class SCheck: UIButton {
     private var activeTintColor: UIColor = UIColor(red: 48, green: 79, blue: 254, alpha: 1)
     private var identifier: String
     
-    public init(isChecked: Bool = false, id: String = UUID().uuidString) {
+    public init(isChecked: Bool = false, id: String = UUID().uuidString, actColor: UIColor? = nil, inactColor: UIColor? = nil) {
         self.identifier = id
         self.isCheckedChecker = isChecked
+        if let act = actColor { self.activeTintColor = act }
+        if let inact = inactColor { self.inactiveTintColor = inact }
         super.init(frame: CGRect(x: 0, y: 0, width: 24, height: 24))
         self.addTarget(nil, action: #selector(didTapOnCheck), for: .touchUpInside)
+        reset()
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    public func set(isChecked: Bool? = nil, id: String? = nil, actCol: UIColor? = nil, inactCol: UIColor? = nil) {
+        if let id = id { self.identifier = id }
+        if let ch = isChecked { self.isCheckedChecker = ch }
+        if let act = actCol { self.activeTintColor = act }
+        if let inact = inactCol { self.inactiveTintColor = inact }
+        reset()
+    }
     
     public var isChecked: Bool {
         get { return isCheckedChecker }
