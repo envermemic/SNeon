b75eb09920205faa9ac572431249a23f5e0c68b9
Enver <enver.memic80@gmail.com>
2019-10-24 09:41:21 +0200

    Update STagLabel.swift

--- SNeon/STagLabel.swift
+++ SNeon/STagLabel.swift
@@ -7,8 +7,194 @@
 //
 
 import UIKit
+import Neon
+
+public extension STagLabel {
+    
+    /// set tag view title and conf width
+    var title: String? {
+        get { return self.title_ }
+        set {
+            self.title_ = newValue
+            setup()
+        }
+    }
+    
+    var uppercassed: Bool {
+        get { return self.uppercassed_ }
+        set {
+            if newValue != self.uppercassed_ {
+                self.uppercassed_ = newValue
+                setup()
+            }
+        }
+    }
+    
+    /// subvies will be removed from content view and set to default
+    func reset() {
+        self.label.text = nil
+        self.iconView.image = nil
+        for v in contentView.subviews { v.removeFromSuperview() }
+        label.textAlignment = .left
+        
+        self.title_ = nil
+        self.font_ = UIFont.systemFont(ofSize: 14, weight: .medium)
+        self.icon_ = nil
+        self.titleColor_ = .black
+        self.iconTintColor_ = nil
+        self.groupTo_ = .center
+        self.uppercassed_ = false
+        self.borderColor_ = nil
+        self.borderWidth_ = nil
+        self.radius_ = 0
+        self.height_ = 48
+        self.off = 12
+        self.ins = 8
+        label.font = self.font_
+        
+        self.setup()
+    }
+    
+    func set(titleColor: UIColor? = nil, tintColor: UIColor? = nil, backgroundColor: UIColor? = nil,
+             radius: CGFloat? = nil, borderColor: UIColor? = nil, borderWidth: CGFloat? = nil,
+             h: CGFloat? = nil, font: UIFont? = nil,
+             fixIconSize: CGSize? = nil, xOffset: CGFloat? = nil, xInset: CGFloat? = nil) {
+        
+        if let titleColor = titleColor { self.titleColor_ = titleColor }
+        if let tintColor = tintColor { self.iconTintColor_ = tintColor }
+        if let backColor = backgroundColor { self.backgroundColor_ = backColor }
+        if let radius = radius { self.radius_ = radius }
+        if let bc = borderColor { self.borderColor_ = bc }
+        if let bw = borderWidth { self.borderWidth_ = bw }
+        if let h = h { self.height_ = h }
+        if let font = font { self.font_ = font }
+        if let fixIS = fixIconSize { self.iconView.frame.size = fixIS } else { self.iconView.frame.size = CGSize(width: 24, height: 24) }
+        if let xOff = xOffset { self.off = xOff }
+        if let inset = xInset { self.ins = inset }
+        self.setup()
+    }
+    
+    /// Setup and configure icon
+    /// - Parameter tintColor: tintColor UIColor or nil for reset
+    /// - Parameter imageName: name: String or nil for reset
+    func setIcon(tintColor: UIColor?, imageName: String?) {
+        if let name = imageName {
+            self.iconTintColor_ = tintColor
+            self.icon_ = name
+        } else {
+            icon_ = nil
+            iconTintColor_ = nil
+        }
+        setup()
+    }
+    
+    private func setup() {
+        
+        // setup icon image view
+        if let iconName = icon_ {
+            if self.iconView.superview == nil { contentView.addSubview(iconView) }
+            self.iconView.isHidden = false
+            if let color = self.iconTintColor_ {
+                self.iconView.image = UIImage(named: iconName)?.withRenderingMode(.alwaysTemplate)
+                self.iconView.tintColor = color
+            } else {
+                self.iconView.image = UIImage(named: iconName)?.withRenderingMode(.alwaysOriginal)
+            }
+        } else {
+            if self.iconView.superview != nil { self.iconView.removeFromSuperview() }
+            self.iconView.isHidden = true
+            self.iconView.image = nil
+        }
+        
+        // set text
+        if let ttl = self.title_ {
+            if self.label.superview == nil { contentView.addSubview(label) }
+            self.label.isHidden = false
+            self.label.font = self.font_
+            self.label.text = ttl
+            self.label.textColor = self.titleColor_
+            self.label.textAlignment = groupTo_ == .right ? .right : .left
+        } else {
+            if self.label.superview != nil { self.label.removeFromSuperview() }
+            self.label.isHidden = true
+        }
+        
+        // setup frame
+        self.frame.size.width = self.off * 2
+        if !self.iconView.isHidden { self.frame.size.width += self.iconView.width }
+        if let ttl = self.title_ {
+            let labelW = ttl.width(font: self.font_)
+            self.frame.size.width += labelW
+            self.label.frame.size = CGSize(width: labelW, height: 24)
+        }
+        if !self.iconView.isHidden && !self.label.isHidden { self.frame.size.width += self.ins }
+        
+        self.frame.size.height = self.height_
+        
+        // setup border
+        if let bw = self.borderWidth_, let bc = borderColor_ {
+            self.contentView.layer.borderWidth = bw
+            self.contentView.layer.borderColor = bc.cgColor
+        } else {
+            self.contentView.layer.borderWidth = 0
+            self.contentView.layer.borderColor = nil
+        }
+        
+        if self.radius_ < 0 { self.radius_ = -1 }
+        let rd: CGFloat = self.radius_ < 0 || self.radius_ >= self.height / 2 ? self.height / 2 : self.radius_
+        self.layer.cornerRadius = rd
+        self.clipsToBounds = true
+        
+        self.contentView.backgroundColor = self.backgroundColor_
+        self.layout()
+    }
+}
+
 
 public class STagLabel: UIView {
+    
+    ///
+    private var title_: String?
+    
+    ///
+    private var font_: UIFont = UIFont.systemFont(ofSize: 14, weight: .medium)
+    
+    ///
+    private var icon_: String?
+    
+    ///
+    private var titleColor_: UIColor = .black
+    
+    ///
+    private var iconTintColor_: UIColor?
+    
+    ///
+    private var backgroundColor_: UIColor = UIColor.white
+    
+    ///
+    private var groupTo_: Pos = .left
+    
+    ///
+    private var uppercassed_: Bool = true
+    
+    ///
+    private var borderColor_: UIColor?
+    
+    ///
+    private var borderWidth_: CGFloat?
+    
+    ///
+    private var radius_: CGFloat = 0
+        
+    ///
+    private var height_: CGFloat = 48
+    
+    ///
+    private var off: CGFloat = 12
+    
+    ///
+    private var ins: CGFloat = 8
+    
     
     private var label = UILabel()
     private var iconView = UIImageView()
@@ -25,103 +211,16 @@ public class STagLabel: UIView {
     
     public override func layoutSubviews() {
         super.layoutSubviews()
-        self.setupLayouts()
-    }
-    
-    /// subvies will be removed from content view and set to default
-    public func reset() {
-        for v in contentView.subviews { v.removeFromSuperview() }
-        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
-        label.textAlignment = .center
-        self.setIcon(tintColor: nil, imageName: nil)
-        self.title = nil
-        self.setLayer()
-    }
-    
-    /// Setup colors
-    /// - Parameter text: text color **default = black**
-    /// - Parameter bckg: background color **default = white**
-    public func colorScheme(text: UIColor = .black, bckg: UIColor = .white) {
-        label.textColor = text
-        contentView.backgroundColor = bckg
-    }
-    
-    /// Setup radius and border
-    /// - Parameter radius: CGFloat ** default = 0, no radius **
-    /// - Parameter border: border params (CGFloat, UIColor) ** by default no border **
-    public func setLayer(radius: CGFloat = 0, border: (CGFloat, UIColor) = (0,.clear)) {
-        clipsToBounds = true
-        layer.cornerRadius = radius
-        layer.borderWidth = border.0
-        layer.borderColor = border.1 == .clear ? nil : border.1.cgColor
-    }
-    
-    /// set tag view title and conf width
-    public var title: String? {
-        get { return label.text ?? "" }
-        set {
-            label.text = newValue
-            if let ttl = newValue {
-                if ttl != "" && ttl != " " {
-                    if label.superview == nil { contentView.addSubview(label) }
-                    setupLayouts()
-                    return
-                }
-            }
-            if label.superview != nil { label.removeFromSuperview() }
-        }
-    }
-    
-    /// Setup and configure icon
-    /// - Parameter tintColor: tintColor UIColor or nil for reset
-    /// - Parameter imageName: name: String or nil for reset
-    public func setIcon(tintColor: UIColor?, imageName: String?) {
-        if let name = imageName {
-            if iconView.superview == nil { contentView.addSubview(iconView) }
-            if let color = tintColor {
-                iconView.image = UIImage(named: name)?.withRenderingMode(.alwaysTemplate)
-                iconView.tintColor = color
-            }
-            else { iconView.image = UIImage(named: name)?.withRenderingMode(.alwaysOriginal) }
-            setupLayouts()
-            return
-        }
-        if iconView.superview != nil { iconView.removeFromSuperview() }
+        self.layout()
     }
     
     ///
-    private func setupLayouts() {
-        
-        // left icon and right text
-        // 12 + icon 24 + 6 + 2 text offset + TEXT + 2text offset + 10
-        
-        // only icon
-        // 12 + 24 + 12
-        
-        // only text
-        // 10 + 2 text offset + TEXT + 2text offset + 10
-        
-        // from right side to left...
-        var right: CGFloat = 12
-        if label.superview != nil && title != " " && title != "" {
-            if let text = title {
-                right += 2
-                let txt = text.width(font: label.font)
-                label.to_side(.right, pad: right, w: txt + 4, h: 24)
-                right += label.width + 6
-            }
-        }
-        
-        if iconView.superview != nil {
-            iconView.to_side(.left, pad: right, w: 24, h: 24)
-            right += 26
-        }
-        
-        right += 10
-        
-        frame.size.height = 36
-        frame.size.width = right
+    private func layout() {
         contentView.fillSuperview()
+        let edge: Edge = groupTo_ == .right ? .right : .left
+        var offset: CGFloat = self.off
+        if !iconView.isHidden { iconView.to_side(edge, pad: offset); offset += iconView.width + ins }
+        if !label.isHidden { label.to_side(edge, pad: offset) }
     }
 }
 
